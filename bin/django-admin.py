4133a9bc9818bbe1bba848569ae069e452f98d15
JinhyunJeun <jhjeun86>
2018-10-11 20:37:42 +0900

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jhjeun86/anaconda3/envs/virtualEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ test.py
@@ -0,0 +1,2 @@
+msg = "DebugTest"
+print(msg)
