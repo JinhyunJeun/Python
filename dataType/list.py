61f3b29e9755aa3c78733f601b1277610925e382
JinhyunJeun <jhjeun86>
2018-10-14 17:27:16 +0900

    dataType: 문자열, 리스트

---
+++ dataType/list.py
@@ -0,0 +1,137 @@
+# list자료형
+# list indexing
+a = [1, 2, 3]
+print(a)
+print(a[0]+a[2])
+print(a[-1])
+a = [1, 2, 3, ['a', 'b', 'c']]
+print(a[-1][0])
+a = [1, 2, ['a', 'b', ['life', 'is']]]
+print(a[2][2][0])
+
+
+# list slicing
+a = [1, 2, 3, 4, 5]
+print(a[0:2]) # 마지막번째는 포함되지 않는다
+a = "12345"
+print(a[0:2]) # 자바의 substring과 같음
+a = [1, 2, 3, 4, 5]
+b = a[:2]
+c = a[2:]
+print(b, c)
+
+# 중첩된 리스트에서 슬라이싱하기
+a = [1, 2, 3, ['a', 'b', 'c'], 4, 5]
+print(a[2:5])
+print(a[3][:2])
+
+# list연산자: +기호로 더할 수 있고 *기호로 반복할 수 있다.
+# 1) list더하기(+)
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a + b)
+
+# 2) list반복하기(*)
+a = [1, 2, 3]
+print(a*3)
+
+
+# list의 수정, 변경과 삭제
+# 1. 리스트에서 하나의 갑ㅅ 수정하기
+a = [1, 2, 3]
+a[2] = 4
+print(a)
+# 2. 리스트에서 연속된 범위의 갑ㅅ 수정하기
+print(a[1:2])
+a[1:2] = ['a', 'b', 'c']
+print(a)
+# 3. []사용해서 리스트 요소 삭제하기
+a[1:3] = []
+print(a)
+# 4. del함수 사용해 리스트 요소 삭제하기
+del a[1]
+print(a)
+
+# list관련 함수들
+# list에 요소 추가
+a = [1, 2, 3]
+a.append(4)
+print(a)
+a.append([5, 6])
+print(a)
+
+# list 정렬(sort): sort함수는 list의 요소들을 순서대로 정렬해준다.
+a = [1, 4, 3, 2]
+a.sort()
+print(a)
+
+# list 뒤집기(reverse): reverse함수는 list를 역순으로 뒤집어준다. 이때 list요소들을 정렬 후 역순으로 정렬하는 것이 아니라 그냥 현재의
+#                      요소들을 거꾸로 뒤집는다.
+a = ['a', 'c', 'b']
+a.reverse()
+print(a)
+
+# 위치반환(index): index(x) 함수는 리스트에 x라는 갑ㅅ이 있으면 x의 위치갑ㅅ을 반환한다. JAVA의 indexOf와 같음
+a = [1, 2, 3]
+print(a.index(3)) # 해당하는 요소가 없다면 리턴없이 에러를 뿜는다.
+
+# list에 요소 삽입(insert): insert(a, b)는 list의 a번째 위치에 b를 삽입하는 함수이다.
+a.insert(0, 'a')
+print(a)
+
+# list요소 제거(remove): remove(x)는 list에서 첫 번째로 나오는 x를 제거하는 함수이다.
+a = [1, 2, 3, 1, 2, 3]
+a.remove(3)
+print(a)
+
+# list 요소 끄집어내기(pop): pop()은 리스트의 맨 마지막 요소를 돌려주고 그 요소는 삭제하는 함수이다.
+a = [1, 2, 3]
+print(a.pop())
+print(a)
+
+# list에 포함된 요소 x의 갯수 세기(count)
+a = [1, 2, 3, 1]
+print(a.count(1))
+
+# list 확장(extend): extend(x)에는 list만 올 수 있으며 원래의 a list에 x list를 더하게 된다.
+a = [1, 2, 3]
+a.extend([4, 5])
+print(a)
+b = [6,7]
+a.extend(b)
+print(a)
+
+
+
+# 문제1. list indexing, a list를 이용하여 you too 를 출력하시오.
+a = ['life', 'is', 'too', 'short', 'you', 'need', 'python']
+a.reverse()
+print(a[2], a[4])
+
+# 문제2. list join, list b를 life is too short로 출력해보자
+b = ['life','is','too','short']
+print(" ".join(b))
+
+# 문제3. list의 갯수
+a = [1, 2, 3]
+print(len(a))
+
+# 문제4. list의 append, extend / list에 [4,5]를 apend 했을 때와 extend 했을 때의 차이
+a = [1, 2, 3]
+a.append([4,5])
+print(a)
+a = [1, 2, 3]
+a.extend([4,5])
+print(a)
+
+# 문제5. list 정렬
+a = [1, 3, 5, 4, 2]
+a.sort()
+a.reverse()
+print(a)
+
+# 문제6. list 삭제: a = [1, 2, 3, 4, 5]를 a = [1, 3, 5]로 만들어보자
+a = [1, 2, 3, 4, 5]
+a.remove(2)
+a.remove(4)
+print(a)
---
+++ dataType/string.py
@@ -0,0 +1,150 @@
+# 문자열 formating
+# 숫자 바로 대입
+string = "I eat %d apples" % 3
+print(string)
+# 문자 바로 대입
+string = "I eat %s apples" % "five"
+print(string)
+# 숫자 갑ㅅ을 나타내는 변수로 대입
+num = 3
+string = "I eat %d apples" % num
+print(string)
+# 2개 이상의 갑ㅅ 넣기
+num = 10
+day = "three"
+string = "I ate %d apples. So I was sick for %s days" % (num, day)
+print(string)
+# 문자열 format code
+# %s = String
+# %c = Character
+# %d = Integer
+# %f = floating-point
+# %o = 8진수
+# %x = 16진수
+# %% = Literal % (%자체)
+string = "error is %d %%" % 98
+print(string)
+
+
+# 포멧코드와 숫자 함께 사용하기
+# 1) 정렬과 공백
+string = "%10s" % "hi" # 10칸 안에서 문자열 hi를 오른쪽 정렬하라는 의미
+print(string)
+string = "%-10s" % "hi" # 10칸 안에서 문자열 hi를 왼쪽 정렬하라는 의미
+print(string)
+string = "%-10sjane" % "hi" # 10 칸 안에서 hi는 왼쪽 정렬되고 11번째에 jane이 들어가게됨
+print(string)
+# 2) 소수점 표현하기
+floooat = "%0.4f" % 3.42134234 # 소수점 넷째 자리까지 표현
+print(floooat)
+floooat = "%10.4f" % 3.42134234 # 10개짜리 공간에서 오른쪽 정렬하고 소수점 넷째 자리까지 표현
+print(floooat)
+
+
+# format 함수를 이용한 포매팅: 문자열의 format() 함수를 이용하면 좀 더 발전된 스타일로 문자열의 포맷을 지정할 수 있다.
+# 1) 숫자 바로 대입하기
+string = "I eat {0} apples.".format(3)
+print(string)
+# 2) 문자열 바로 대입하기
+string = "I eat {0} apples.".format("three")
+print(string)
+# 3) 숫자 갑ㅅ을 가진 변수로 대입하기
+num = 5
+string = "I eat {0} apples".format(num)
+print(string)
+# 4) 두 개 이상의 갑ㅅ 넣기
+num = 10
+day = "two"
+string = "I ate {0} apples. So I sick for {1} days.".format(num, day)
+print(string)
+# 5) 이름으로 넣기
+string = "I ate {number} apples. So I was sick for {day} days".format(number=12, day="seven") # format 좀 신기한듯
+print(string)
+# 6) 인덱스와 이름을 혼용해서 넣기
+string = "I ate {0} apples. So I was sick for {day} days".format(14, day = 7)
+print(string)
+# 7) 왼쪽 정렬
+string = "{0:<10}".format("hi") # 총 자릿수 10개에서 왼쪽 정렬, 화살표가 가리키는 방향이 정렬 방향이 된다.
+print(string)
+# 8) 오른쪽 정렬
+string = "{0:>10}".format("hi")
+print(string)
+# 9) 가운데 정렬
+string = "{0:^10}".format("hi") # 가운데 정렬은 ^
+print(string)
+# 10) 공백 채우기
+string = "{0:=^10}".format("hi")
+print(string)
+string ="{0:^^10}".format("ss")
+print(string)
+string = "{0:!^10}".format("hi")
+print(string)
+string = "{0:!<10}".format("hi")
+print(string)
+# 소수점 표현하기
+y = 3.42134234
+floooat = "{0:0.4f}".format(y)
+print(floooat)
+floooat = "{0:10.4f}".format(y) # 10칸에서 소수점 네 자리 + 오른쪽 정렬
+print(floooat)
+# { 또는 } 표현하기
+string = "{{and}}".format()
+print(string)
+
+# f문자열 포매팅(python 3.6 이상부터 가능)
+name = "홍길동"
+age = 30
+print(f"나의 이름은 {name} 입니다. 나이는 {age} 입니다.")
+# f문자열 포메팅은 표현식을 지원하기 때문에 아래와 같은 것도 가능하다
+age = 30
+print(f"나는 내년이면 {age+1}살이 된다.")
+# dictionary는 f문자열에서 다음과 같이 사용 가능
+d = {'name':'홍길동', 'age':30}
+print(f'나의 이름은 {d["name"]}입니다. 나이는 {d["age"]}입니다.') # 따옴표 잘 쓸 것. ""와''을 잘 사용할 것
+# 정렬은 다음과 같이 할 수 있다. / 이건 연습이 좀 필요할듯
+print(f'{"hi":<10}') 
+print(f'{"hi":>10}')
+print(f'{"hi":^10}')
+print(f'{"hi":=^10}')
+print(f'{"hi":!<10}')
+# f 문자열에서 { 나 }를 사용하려면
+print(f"{'{'}")
+print(f"{'}'}")
+
+
+# 문자열과 관련 함수들: 내장함수를 사용하려면 문자열 변수 이름 뒤에 .을 붙인 다음 함수 이름을 써주면 된다.
+# 문자 갯수 세기(count)
+a = "hobby"
+print(a.count("b"))
+# 위치 알려주기1: find
+a = "Python is the best choice"
+print(a.find("b")) # 찾는 문자가 없으면 -1을 리턴한다. JAVA의 indexOf와 같음.
+# 위치 알려주기2: index
+a = "Life is too Short"
+print(a.index("i"))
+# 문자열 삽입: join
+a = ","
+print(a.join('abcd'))
+print(",".join("efgh")) # ,에 변수 지정 안하고 이렇게 많이 씀
+print(",".join(['i','j','k','l'])) # join을 쓰면 출력이 list처럼 안나온다.
+# 소문자를 대문자로 바꾸기: upper()
+a = "hi!"
+print(a.upper())
+# 대문자를 소문자로 바꾸기: lower()
+a = "HELLO PYTHON"
+print(a.lower())
+# 왼쪽 공백 지우기: lstrip()
+a = " hi "
+print(a.lstrip())
+# 오른쪽 공백 지우기: rstrip()
+print(a.rstrip())
+# 양쪽 공백 지우기: strip()
+print(a.strip())
+# 문자열 바꾸기: replace()
+a = "life is too short"
+print(a.replace("life","your hair"))
+# 문자열 나누기: split()
+a = "Hello; world"
+print(a.split(";"))
+b = "Hello Python"
+print(b.split())
